358d9829978db5b17e1dca8155f5b589ae7879ec
Christina R <xsnachannell@gmail.com>
2022-05-05 12:50:41 -0700

    adds server.js file and initial code to outline routes that need to be created

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/notes', ) // should reutnr the notes.html file
+
+app.get('api/notes', ) //should read file and return all saved notes as JSON
+
+app.post('/api/notes') //should receive new note and save on the request body and add it to db.json file
